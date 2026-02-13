5f4fb4092cea7cdaf686e989bd7ef384119591ca
xytgy <xytgy99@gmail.com>
2026-02-13 19:50:37 +0800

    feat: 添加 Rust 项目 greeting 的初始实现
    这是一个全新的 Rust 项目，包含基本的 "Hello, world!" 程序。
    - 添加了 Cargo.toml 配置文件，定义了项目名称、版本和 Rust 版本
    - 创建了 main.rs 文件，包含简单的打印输出功能
    - 包含了 Cargo.lock 文件以锁定依赖版本
    - 添加了编译生成的目标文件和构建缓存文件

---
+++ greeting/target/debug/deps/greeting.d
@@ -0,0 +1,5 @@
+E:\software\rust-demo\greeting\target\debug\deps\greeting.d: src\main.rs
+
+E:\software\rust-demo\greeting\target\debug\deps\greeting.exe: src\main.rs
+
+src\main.rs:
---
+++ greeting/target/debug/greeting.d
@@ -0,0 +1,1 @@
+E:\software\rust-demo\greeting\target\debug\greeting.exe: E:\software\rust-demo\greeting\src\main.rs
